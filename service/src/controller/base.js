79a87a2dafb364dccb06cd7c48ef71b4e25a6c1a
Jerryqun <932798303@qq.com>
2022-11-22 20:14:04 +0800

    source api

--- service/src/controller/base.js
+++ service/src/controller/base.js
@@ -1,26 +1,60 @@
+/* eslint-disable quotes */
+/* eslint-disable no-return-await */
 module.exports = class extends think.Controller {
-  __before() {
-
-  }
-  getParams(params) {
+  __before() {}
+  getParams(params, likeKeys) {
     const where = {};
-    Object.keys(params).forEach(key => {
+    Object.keys(params).forEach((key) => {
       if (!think.isEmpty(params[key])) {
-        where[key] = params[key]
+        where[key] = params[key];
       }
-    })
+    });
+
+    likeKeys.forEach((key) => {
+      if (where[key]) {
+        where[key] = ["like", `%${where[key]}%`];
+      }
+    });
+
     return where;
   }
   // 分页查询
-  async commonList(modelName) {
-    const { pageSize = 10, pageNum = 1, ...params } = this.post();
+  async commonList(modelName, likeKeys) {
+    const { pageSize = 10, pageNum = 1, ...params } = this.get();
     const model = this.model(modelName);
     return await model
       .where({
-        ...this.getParams(params),
+        ...this.getParams(params, likeKeys),
       })
       .order("id DESC")
       .page(pageNum, pageSize)
       .countSelect();
+  }
+  // 物理删除
+  async commonRemove(modelName) {
+    const { id } = this.get();
+    const model = this.model(modelName);
+    return await model.where({ id }).delete();
+  }
+  // 通用新增
+  async commonAdd(modelName) {
+    const data = this.post();
+    const model = this.model(modelName);
+    return await model.add({
+      ...data,
+      create_time: ["exp", "CURRENT_TIMESTAMP()"],
+      update_time: ["exp", "CURRENT_TIMESTAMP()"],
+    });
+  }
+  // 通用更新
+  async commonUpdate(modelName) {
+    const data = this.post();
+    const model = this.model(modelName);
+    const params = {
+      ...data,
+      update_time: ["exp", "CURRENT_TIMESTAMP()"],
+    };
+    delete params.createTime;
+    return await model.update(params);
   }
 };
--- service/src/controller/resource.js
+++ service/src/controller/resource.js
@@ -1,12 +1,51 @@
+/* eslint-disable quotes */
 const Base = require("./base.js");
 
 module.exports = class extends Base {
   async listAction() {
-    const data = await this.commonList("app_resource");
+    const data = await this.commonList("app_resource", ["app_name"]);
     this.json({
       code: 200,
       data,
-      mes:'操作成功'
+      mes: "操作成功",
     });
+  }
+
+  async delAction() {
+    const data = await this.commonRemove("app_resource");
+    this.json({
+      code: 200,
+      data,
+      mes: "操作成功",
+    });
+  }
+
+  async addAction() {
+    const { path_name, id } = this.post();
+    if (id) {
+      await this.commonUpdate("app_resource");
+      this.json({
+        code: 200,
+        data: id,
+      });
+      return;
+    }
+    const exit = await this.model("app_resource")
+      .where({
+        path_name,
+      })
+      .find();
+    if (think.isEmpty(exit)) {
+      const insertId = await this.commonAdd("app_resource");
+      this.json({
+        code: 200,
+        data: insertId,
+      });
+    } else {
+      this.json({
+        code: 400,
+        msg: "资源路径已暂用",
+      });
+    }
   }
 };
