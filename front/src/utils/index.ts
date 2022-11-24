cc618f2c657d54915d9476a19c9c601907346ef8
lighthouse <lighthouse@123.com>
2022-11-24 19:55:13 +0800

    DEFAULTHTML

--- front/src/utils/index.ts
+++ front/src/utils/index.ts
@@ -1,4 +1,24 @@
 /**
  * iconUrl
  */
- export const iconUrl = '//at.alicdn.com/t/c/font_3765716_4gpu5d5pk98.js';
+
+ export const iconUrl = "//at.alicdn.com/t/c/font_3765716_4gpu5d5pk98.js";
+
+ export const STATICURL = "http://1.15.184.206:9000/";
+ 
+ export const DEFAULTHTML = `<!DOCTYPE html>
+ <html>
+ <head>
+   <meta charset="utf-8" />
+   <title>title</title>
+   <meta http-equiv="x-ua-compatible" content="ie=edge,chrome=1" />
+   <meta name="viewport" content="width=device-width" />
+   <link rel="icon" href="https://yun-static.cdn.bcebos.com/yun-dev-tools/favicon.png">
+   <script defer src="https://yun-static.cdn.bcebos.com/xxx/x.x.x.js"></script>
+   <link href="https://yun-static.cdn.bcebos.com/xxx/x.x.x.css" rel="stylesheet">
+ </head>
+ <body>
+   <div id="ice-container"></div>
+ </body>
+ </html>`;
+ 
