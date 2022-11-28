e3b8cc35a5eec33b1d82b39a6886555e71fc5aa0
lighthouse-8G <lighthouse@8g.com>
2022-11-28 16:28:23 +0800

    fix

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { history } from "ice";
+import Resource from './resource'
 
 export default function Home() {
   history?.push("/resource");
-  return <div>home</div>;
+  return <Resource/>;
 }
